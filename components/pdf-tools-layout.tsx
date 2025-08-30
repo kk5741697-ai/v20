6f5b216b437f1ec9a4c0b7a30cd57c0ebbeed288
skitto01341193953-droid <skitto.01341193953@gmail.com>
2025-08-30 20:49:55 +0600

    Updated pdf-tools-layout.tsx

--- components/pdf-tools-layout.tsx
+++ components/pdf-tools-layout.tsx
@@ -512,7 +512,7 @@ export function PDFToolsLayout({
 
       {/* Top Ad Banner */}
       <div className="bg-white">
-        <div className="container mx-auto px-4 py-1 lg:py-2">
+        <div className="container mx-auto px-4 py-1 lg:py-3">
           <AdBanner 
             adSlot="tool-header-banner"
             adFormat="auto"

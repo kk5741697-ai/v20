66235711bb8d3824e75f424094c26d5fbe100875
skitto01341193953-droid <skitto.01341193953@gmail.com>
2025-08-30 20:22:29 +0600

    Updated image-tools-layout.tsx

--- components/image-tools-layout.tsx
+++ components/image-tools-layout.tsx
@@ -535,7 +535,7 @@ export function ImageToolsLayout({
             <p className="text-base lg:text-lg text-muted-foreground max-w-2xl mx-auto px-4">{description}</p>
           </div>
 
-          <div className="max-w-2xl mx-auto">
+          <div className="max-w-4xl mx-auto">
             <div 
               className="border-2 border-dashed border-gray-300 rounded-2xl flex flex-col items-center justify-center text-gray-500 cursor-pointer hover:border-blue-400 hover:bg-blue-50/30 transition-all duration-300 p-6 lg:p-8 group"
               onDrop={handleDrop}
